5bae6a6e91d276f6f1e2e4b57cd158aaa76071e2
nosce <claudia.kutter@gmx.net>
2018-06-11 19:53:59 +0200

    Added some comments

--- TicTacToe/training_results/plot_results.py
+++ TicTacToe/training_results/plot_results.py
@@ -1,23 +1,35 @@
+"""
+This script reads the file with the stored results and shows on the console:
+- Clock time and CPU time required for training
+- Number of entries in a player's Q-table and how many entries were updated yet
+- Number of games won / lost / drawn
+- Total reward of both players
+- Average reward of both players
+- Average number of moves of both players
+The cumulative reward over time is shown in a graph.
+"""
+__author__ = "Claudia Kutter"
+
 import json
 import collections as coll
 import matplotlib.pyplot as plt
 
-# SPECIFY FILE HERE
+# SPECIFY FILES HERE
 file = 'ttt_results_20000_SARSA-vs-SARSA.json'
-q_table = 'ttt_table_SARSA_20000.json'
+q_table = 'ttt_table_X_SARSA_20000.json'
 
-# Read in results file
+# Read in files
 with open(file) as f:
 	results = json.load(f)
 with open(q_table) as f:
 	table = json.load(f)
 
-# Convert keys to int
+# Convert episode keys to int
 results = {int(k): v for k, v in results.items()}
 # Sort
 results = coll.OrderedDict(sorted(results.items()))
 
-# Extract data
+# Data variables
 length = len(results) - 1
 episodes = list(results.keys())
 episodes.pop(0)
@@ -30,6 +42,8 @@ moves_x = 0
 moves_o = 0
 rewards_x = []
 rewards_o = []
+
+# Extract data
 for key, value in results.items():
 	if key == -1:
 		print("Elapsed time: {} sec".format(value['clock-time']))
@@ -50,11 +64,13 @@ for key, value in results.items():
 		total_reward_x = value['X']['reward']
 		total_reward_o = value['O']['reward']
 
+# Count updated entries in Q-table
 init_value = 0
 for item in table.values():
 	if item == 0.1:
 		init_value += 1
 
+# Print results to console
 print("Entries in Q-table: ", len(table))
 print("Initialized entries: ", init_value)
 print("Updated entries: ", len(table) - init_value)
@@ -69,6 +85,7 @@ print("Total reward of Player O after {} games: {}".format(length, total_reward_
 print("Average reward of Player O: ", total_reward_o / length)
 print("Average moves of Player O: ", moves_o / length)
 
+# Plot cumulative reward over time
 plt.plot(episodes, rewards_x, label='Player X', color='orange')
 plt.plot(episodes, rewards_o, label='Player O', color='blue')
 plt.ylabel('Cumulative reward')
